58254825fd34d982c50a580ca93423ad13c1cd9a
Michael Brandt <michael.j.brandt@colorado.edu>
2014-02-19 09:54:41 -0700

    slate fixes

--- .slate.js
+++ .slate.js
@@ -1,1 +1,14 @@
-// var success = S.src(".sizeup.slate.js");
+S.cfga({
+  "defaultToCurrentScreen": true,
+
+  "nudgePercentOf": "screenSize",
+  "resizePercentOf": "screenSize",
+
+  "windowHintsShowIcons": true,
+  "windowHintsIgnoreHiddenWindows": false,
+  "windowHintsSpread": true
+});
+
+S.bnda({
+   "w:ctrl;alt": S.op("hint") hint ASDFGQWERTZXCVBHJKLYUIOPNM
+});
