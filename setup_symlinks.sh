5cce0efca26ef61d14fe6c1f1c58a0628bc6bf90
Michael Brandt <mbrandt@colorado.edu>
2015-12-29 14:48:16 -0700

    Add script to create links, autosquash on rebase

---
+++ setup_symlinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for fn in .bashrc .bash_profile .sizeup.slate .slate .slate.js .gitconfig; do
+    echo "linking $fn in $HOME"
+    rm -f $HOME/$fn
+    ln -sf $THIS_DIR/$fn $HOME/$fn
+done
+
+echo "linking .gitignore_global in $HOME/.gitignore"
+rm -f $HOME/.gitignore
+ln -sf $THIS_DIR/.gitignore_global $HOME/.gitignore
