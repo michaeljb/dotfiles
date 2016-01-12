0f8f7c6b0bb462ae3f8169c8b455eefca7220576
Michael Brandt <mbrandt@colorado.edu>
2016-01-12 10:53:43 -0700

    Add flake8 config that works with elpy
    From the [elpy wiki][0] on github:
    
    elpy uses flake8 for syntax checking. This is a wrapper around pep8,
    PyFlake and Ned Batchelder’s McCabe script. You can change the flake8
    settings globally by editing its config file. For all [non-windows]
    operating systems it is `~/.config/flake8`.
    
    [0][https://github.com/jorgenschaefer/elpy/wiki/Configuration]

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -8,6 +8,11 @@ for fn in .bashrc .bash_profile .slate .slate.js .gitconfig .tmux.conf .git-prom
     ln -sf $THIS_DIR/$fn $HOME/$fn
 done
 
+echo "linking flake8 in $HOME/.config/flake8"
+rm -f $HOME/.config/flake8
+mkdir $HOME/.config
+ln -sf $THIS_DIR/flake8 $HOME/.config/flake8
+
 echo "linking .gitignore_global in $HOME/.gitignore"
 rm -f $HOME/.gitignore
 ln -sf $THIS_DIR/.gitignore_global $HOME/.gitignore
