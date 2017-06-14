deeef05f4168656a91b75d03e35385e353009047
Michael Brandt <michaelbrandt5@gmail.com>
2017-06-13 21:26:14 -0600

    add ssh service restart to dev-vm.sh

--- dev-vm.sh
+++ dev-vm.sh
@@ -47,3 +47,5 @@ sudo apt-get install -y docker-ce
 
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo systemctl restart sshd.service
